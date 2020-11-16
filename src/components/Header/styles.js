c72e4767da6433cd66622a9661133be79f12d0dd
John Nguyen <jnguyen4103@gmail.com>
2020-11-16 00:30:12 -0500

    Make SearchBar from InputBase and add custom styling.

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -5,6 +5,13 @@ const styles = {
   headerIcon: css`
     padding-right: ${theme.spacing(1)}px;
   `,
+  toolBar: css`
+    align-items: center;
+  `,
+  headerTypography: css`
+    flex: 1;
+    color: ${theme.palette.primary.contrastText};
+  `,
 };
 
 export default styles;
